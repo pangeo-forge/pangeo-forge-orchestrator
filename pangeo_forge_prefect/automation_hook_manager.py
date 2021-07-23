92dded1f011cdc8786e77ba9a37219850c00049d
sharkinsspatial <sharkinsgis@gmail.com>
2021-07-22 20:42:09 -0400

    Only create Prefect hook when it does not already exist.

--- pangeo_forge_prefect/automation_hook_manager.py
+++ pangeo_forge_prefect/automation_hook_manager.py
@@ -61,6 +61,21 @@ def new_automation(flow_group_id, pat_token):
 
 
 def create_automation(flow_id, pat_token):
+    """
+    Create a Prefect Automation to call a Github repository webhook.
+
+    Create a Prefect Automation that POSTs to a Github repository webhook
+    when a test flow is registered.  The Github Action usess the flow_run_id
+    (which is set to the trigger comment's id) to update the comment when the
+    test flow run succeeds or fails.
+
+    Parameters
+    ----------
+    flow_id : str
+        The id of the registered flow
+    pat_token : str
+        A Github PAT token with at a minimum the repo scope.
+    """
     flow_group_id_query = {
         "query": {
             with_args("flow", {"where": {"id": {"_eq": flow_id}}}): {
@@ -71,4 +86,15 @@ def create_automation(flow_id, pat_token):
     }
     flow_group_id_response = client.graphql(flow_group_id_query)
     flow_group_id = flow_group_id_response["data"]["flow"][0]["flow_group_id"]
-    new_automation(flow_group_id, pat_token)
+
+    hook_query = {
+        "query": {
+            with_args(
+                "hook", {"where": {"event_tags": {"_contains": {"flow_group_id": [flow_group_id]}}}}
+            ): {"id"}
+        }
+    }
+    hook_query_response = client.graphql(hook_query)
+    hook_exists = len(hook_query_response["data"]["hook"]) > 0
+    if not hook_exists:
+        new_automation(flow_group_id, pat_token)
