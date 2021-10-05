c7e439ee255740ae6ed051b7d5cfb378f42489eb
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-10-05 12:44:28 -0700

    add path to built-logs table

--- pangeo_forge_orchestrator/cli/bakery.py
+++ pangeo_forge_orchestrator/cli/bakery.py
@@ -45,6 +45,7 @@ def ls(
             table.add_column("Timestamp", style="blue")
             table.add_column("Feedstock", style="green")
             table.add_column("Recipe", style="red")
+            table.add_column("Path", style="cyan")
 
             k = list(bakery_dict[bakery_id]["targets"].keys())[0]
             target = bakery_dict[bakery_id]["targets"][k]
@@ -55,7 +56,13 @@ def ls(
                 logs = json.loads(read_json)
                 ids = list(logs.keys())
                 rows = [
-                    [k, logs[k]["timestamp"], logs[k]["feedstock"], logs[k]["recipe"]]
+                    [
+                        k,
+                        logs[k]["timestamp"],
+                        logs[k]["feedstock"],
+                        logs[k]["recipe"],
+                        logs[k]["path"],
+                    ]
                     for k in reversed(logs.keys())
                 ]
                 if not feedstock_name:
