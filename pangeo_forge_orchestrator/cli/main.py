caadae07cb5d7c02bd15e5276d791ea51ed2dd35
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-10-06 14:15:53 -0700

    restructure top level commands

--- pangeo_forge_orchestrator/cli/main.py
+++ pangeo_forge_orchestrator/cli/main.py
@@ -1,16 +1,10 @@
 import typer
 
-from . import bakery, catalog
+from . import bakery, catalog, feedstock, recipe
 
 app = typer.Typer()
 
 app.add_typer(bakery.app, name="bakery")
 app.add_typer(catalog.app, name="catalog")
-
-
-@app.command()
-def lint(path: str):
-    """
-    Lint a recipe
-    """
-    typer.echo(f"Linting recipe {path}")
+app.add_typer(feedstock.app, name="feedstock")
+app.add_typer(recipe.app, name="recipe")
---
+++ pangeo_forge_orchestrator/cli/recipe.py
@@ -0,0 +1,10 @@
+import typer
+
+app = typer.Typer()
+
+@app.command()
+def lint(path: str):
+    """
+    Lint a recipe
+    """
+    typer.echo(f"Linting recipe {path}")
