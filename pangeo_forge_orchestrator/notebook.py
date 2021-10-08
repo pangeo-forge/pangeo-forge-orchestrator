4d9051a93b440fe10e5a0849636733bc31f9bcf1
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-10-07 17:44:40 -0700

    minimal locally working version of papermill

--- pangeo_forge_orchestrator/notebook.py
+++ pangeo_forge_orchestrator/notebook.py
@@ -1,14 +1,13 @@
-import tempfile
-from dataclasses import dataclass, field
+# import tempfile
+# from dataclasses import dataclass
 
 import papermill as pm
 
+# @dataclass
+# class ExecuteNotebook
 
-class Notebook
 
-    bakery_database: str = 
-
-    def execute_notebook(template_path, parameters):
-        outpath = tempfile.NamedTemporaryFile()
-        pm.execute_notebook(template_path, outpath.name, parameters=parameters)
-        return outpath.name
+def execute(template_path, outfile, parameters):
+    # outfile = tempfile.NamedTemporaryFile(suffix=".ipynb")
+    pm.execute_notebook(template_path, outfile, parameters=parameters)
+    # return outfile.name
