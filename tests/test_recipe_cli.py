9d28da9d402ab847cf20d6683653c62a54db991e
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-10-13 15:00:08 -0700

    correct import in recipe test module

--- tests/test_recipe_cli.py
+++ tests/test_recipe_cli.py
@@ -1,6 +1,6 @@
 import pytest
 
-from .cli_test_funcs import check_output
+from .check_stdout import check_stdout
 
 subcommands = {
     "lint path-to-a-recipe": "Linting recipe path-to-a-recipe",
@@ -10,4 +10,4 @@ subcommands = [(cmd, output) for cmd, output in subcommands.items()]
 
 @pytest.mark.parametrize("subcmd", subcommands)
 def test_recipe_lint(subcmd):
-    check_output(subcmd, module="recipe", drop_chars=("\n"))
+    check_stdout(subcmd, module="recipe", drop_chars=("\n"))
