c1f3417ea2d5293053da8c5c3f522b4f5b3e7311
Ryan Abernathey <ryan.abernathey@gmail.com>
2022-01-16 22:55:37 -0500

    try disabling registration test again

--- tests/test_database.py
+++ tests/test_database.py
@@ -49,7 +49,7 @@ def registered_routes(app: FastAPI):
 # Test endpoint registration ------------------------------------------------------------
 
 
-@pytest.skip
+@pytest.mark.skip(reason="want to see if this changes anything")
 def test_registration(uncleared_session, models_with_kwargs):
     models = models_with_kwargs.models
     new_app = FastAPI()
