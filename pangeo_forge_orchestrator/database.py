5eb8fbae04dcc3c7e540fc81b82178a0d165698d
Ryan Abernathey <ryan.abernathey@gmail.com>
2022-01-16 23:05:10 -0500

    workaround Heroku database uri problem

--- pangeo_forge_orchestrator/database.py
+++ pangeo_forge_orchestrator/database.py
@@ -8,5 +8,9 @@ print(f"DATABASE_URL: {database_url}")
 connect_args = {}
 if database_url.startswith("sqlite:"):
     connect_args = {"check_same_thread": False}
+elif database_url.startswith("postgres://"):
+    # Fix Heroku's incompatible postgres database uri
+    # https://stackoverflow.com/a/67754795/3266235
+    database_url = database_url.replace("postgres://", "postgresql://", 1)
 
 engine = create_engine(database_url, echo=True, connect_args=connect_args)
