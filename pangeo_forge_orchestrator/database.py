0c84a2080b0bbc19185d6ee47074b9ecf53ff047
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-01-24 13:41:04 -0800

    read postgres timestamps in UTC

--- pangeo_forge_orchestrator/database.py
+++ pangeo_forge_orchestrator/database.py
@@ -4,13 +4,15 @@ from sqlmodel import SQLModel, create_engine  # noqa: F401
 
 database_url = os.environ["DATABASE_URL"]
 
-connect_args = {}
 if database_url.startswith("sqlite:"):
     connect_args = {"check_same_thread": False}
 elif database_url.startswith("postgres://"):  # pragma: no cover
     # Fix Heroku's incompatible postgres database uri
     # https://stackoverflow.com/a/67754795/3266235
     database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+if database_url.startswith("postgresql:"):
+    connect_args = dict(options="-c timezone=utc")  # type: ignore
 
 engine = create_engine(database_url, echo=True, connect_args=connect_args)
 
