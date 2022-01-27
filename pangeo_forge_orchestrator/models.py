a0d230fd13d253666f8d97da0806c5a7d6466926
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-01-26 16:48:12 -0800

    doctring for DatasetBase

--- pangeo_forge_orchestrator/models.py
+++ pangeo_forge_orchestrator/models.py
@@ -71,8 +71,12 @@ class DatasetType(str, Enum):
 
 
 class DatasetBase(SQLModel):
-    """
+    """Information about a Pangeo Forge dataset.
 
+    :param name: The name of the dataset.
+    :param reciperun_id: A foreign key for the recipe run which produced this dataset.
+    :param type: An dataset type. Must be one of the options defined in ``DatasetType``.
+    :param public_url: The publically accessible URL at which this dataset can be accessed.
     """
 
     name: str
