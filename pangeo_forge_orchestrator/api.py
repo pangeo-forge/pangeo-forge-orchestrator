102663a8eed088fc3ad69fe8060783388ed53bc6
Ryan Abernathey <ryan.abernathey@gmail.com>
2022-01-29 16:44:14 -0500

    add app metadata

--- pangeo_forge_orchestrator/api.py
+++ pangeo_forge_orchestrator/api.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 
 from .database import maybe_create_db_and_tables
+from .metadata import app_metadata
 from .routers.model_router import router as model_router
 from .routers.security import api_key_router
 from .security import create_admin_api_key
 
-app = FastAPI()
+app = FastAPI(**app_metadata)
 
 
 @app.on_event("startup")
---
+++ pangeo_forge_orchestrator/metadata.py
@@ -0,0 +1,17 @@
+# flake8: noqa
+
+_description = """
+![Pangeo Forge Icon](https://raw.githubusercontent.com/pangeo-forge/pangeo-forge-logo/main/pangeo-forge-logo-blue.svg)
+
+Welcome to the Pangeo Forge API documentation. 🚀
+"""
+
+app_metadata = dict(
+    title="Pangeo Forge API",
+    description=_description,
+    contact={
+        "name": "Pangeo Forge Team",
+        "url": "https://pangeo-forge.org/contact",
+        "email": "help@pangeo-forge.org",
+    },
+)
