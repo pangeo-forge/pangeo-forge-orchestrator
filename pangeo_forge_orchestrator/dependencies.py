c31558298c341eb21c6662722c57aa4923e116b0
Ryan Abernathey <ryan.abernathey@gmail.com>
2022-01-30 21:58:02 -0500

    fix pre-commit

--- pangeo_forge_orchestrator/dependencies.py
+++ pangeo_forge_orchestrator/dependencies.py
@@ -1,2 +1,2 @@
-from .database import get_session
-from .security import check_authentication_header, check_authentication_header_admin
+from .database import get_session  # noqa: F401
+from .security import check_authentication_header, check_authentication_header_admin  # noqa: F401
