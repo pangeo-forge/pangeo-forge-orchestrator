f9228b9eec700c04f8c60af5547d1b35a6161c3a
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-08-08 17:23:37 -0700

    test receive webhook, unauthorized

--- tests/test_github_app.py
+++ tests/test_github_app.py
@@ -1,3 +1,4 @@
+import json
 import os
 from dataclasses import dataclass
 from typing import List, Optional
@@ -384,3 +385,31 @@ async def test_get_delivery(
         response = await async_app_client.get(f"/github/hooks/deliveries/{id_}")
         assert response.status_code == 200
         assert response.json() == delivery
+
+
+@pytest.mark.parametrize(
+    "hash_signature_problem,expected_response_detail",
+    [
+        ("missing", "Request does not include a GitHub hash signature header."),
+        ("incorrect", "Request hash signature invalid."),
+    ],
+)
+@pytest.mark.asyncio
+async def test_receive_github_hook_unauthorized(
+    async_app_client,
+    hash_signature_problem,
+    expected_response_detail,
+):
+    if hash_signature_problem == "missing":
+        headers = {}
+    elif hash_signature_problem == "incorrect":
+        os.environ["GITHUB_WEBHOOK_SECRET"] = "foobar"
+        headers = {"X-Hub-Signature-256": "abcdefg"}
+
+    response = await async_app_client.post(
+        "/github/hooks/",
+        json={},
+        headers=headers,
+    )
+    assert response.status_code == 401
+    assert json.loads(response.text)["detail"] == expected_response_detail
