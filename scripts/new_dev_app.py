ccc7f547fddbe18b7385117794562c3262a3d6d7
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-08-17 22:28:45 -0700

    new_dev_app script improvements

--- scripts/new_dev_app.py
+++ scripts/new_dev_app.py
@@ -1,41 +1,39 @@
+import http.server
 import json
 import os
-import subprocess
+import socketserver
 import sys
-import webbrowser
-from urllib.parse import urlparse
+from urllib.parse import parse_qs, urlparse
 
-cachedir = f"{os.getcwd()}/.github_app_manifest_flow"
+import requests  # type: ignore
+import yaml
 
+CACHEDIR = f"{os.getcwd()}/.github_app_manifest_flow"
+AUTHORIZE = "authorize.html"
+REDIRECT = "redirect.html"
+PORT = 3000
 
-def create_redirect_page():
-    outpath = f"{cachedir}/redirect.html"
-    content = """
-    <html>
-        You've been redirected
-        Your code is <input id="conversion_code">
-        <script>
-            input = document.getElementById("conversion_code")
-            const urlParams = new URLSearchParams(window.location.search);
-            const conversion_code = urlParams.get('code');
-            input.value = conversion_code
-        </script>
-    </html>
-    """
-
-    with open(outpath, "w") as f:
-        f.write(content)
-
-    return "http://localhost:3000/redirect.html"
+if not os.path.exists(CACHEDIR):
+    os.mkdir(CACHEDIR)
 
 
 def main(smee_proxy_url):
     """ """
-    os.mkdir(cachedir)
-    redirect_url = create_redirect_page()
+
+    # Write the redirect page to disk
+    with open(f"{CACHEDIR}/{REDIRECT}", "w") as f:
+        f.write("<html>You've been redirected</html>")
+
+    # We're going to serve from the `CACHEDIR` as base,
+    # so the redirect page will be at this address
+    redirect_url = f"http://localhost:{PORT}/{REDIRECT}"
+
     p = urlparse(smee_proxy_url)
     abbreviated_smee_channel_id = p.path[1:8]
     proxy_url_without_scheme = p.netloc + p.path
+
+    # For manifest parameters docs, see:
+    # https://docs.github.com/en/developers/apps/building-github-apps/creating-a-github-app-from-a-manifest#github-app-manifest-parameters
     manifest = dict(
         name=f"pangeo-forge-dev-{abbreviated_smee_channel_id}",
         url=f"https://pangeo-forge.org/?orchestratorEndpoint={proxy_url_without_scheme}",
@@ -58,7 +56,10 @@ def main(smee_proxy_url):
             "pull_requests": "write",
         },
     )
-    page = f"""
+
+    # This page content adapted from official GitHub docs, here:
+    # https://docs.github.com/en/developers/apps/building-github-apps/creating-a-github-app-from-a-manifest#examples
+    content = f"""
     <html>
         <form action="https://github.com/settings/apps/new?state=abc123" method="post">
             Create a GitHub App Manifest: <input type="text" name="manifest" id="manifest"><br>
@@ -71,17 +72,54 @@ def main(smee_proxy_url):
     <html>
     """
 
-    outpath = "page.html"
+    with open(f"{CACHEDIR}/{AUTHORIZE}", "w") as f:
+        f.write(content)
 
-    with open(f"{cachedir}/{outpath}", "w") as f:
-        f.write(page)
-
-    return outpath
+    return f"http://localhost:{PORT}/{AUTHORIZE}"
 
 
 if __name__ == "__main__":
-    cmd = f"python3 -m http.server 3000 --bind 127.0.0.1 --directory {cachedir}".split()
-    p = subprocess.Popen(cmd, cwd=os.getcwd())
+
+    user_token = os.environ.get("GITHUB_PAT", None)
+    if not user_token:
+        raise ValueError("Env variable 'GITHUB_PAT' required, but unset. ")
+
     smee_proxy_url = sys.argv[1]
-    outpath = main(smee_proxy_url)
-    webbrowser.open(f"http://localhost:3000/{outpath}")
+    authorize_url = main(smee_proxy_url)
+
+    class Handler(http.server.SimpleHTTPRequestHandler):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, directory=CACHEDIR, **kwargs)
+
+        def do_GET(self):
+            p = urlparse(self.path)
+            if "code" in p.query and p.path == f"/{REDIRECT}":
+                # For docs on exchanging temporary code for app config, see:
+                # https://docs.github.com/en/developers/apps/building-github-apps/creating-a-github-app-from-a-manifest#3-you-exchange-the-temporary-code-to-retrieve-the-app-configuration
+                code = parse_qs(p.query)["code"][0]
+                response = requests.post(
+                    f"https://api.github.com/app-manifests/{code}/conversions",
+                    headers={
+                        "Authorization": f"token {user_token}",
+                        "Accept": "application/vnd.github+json",
+                    },
+                )
+                response.raise_for_status()
+                response_json = response.json()
+                app_config = {
+                    "GitHubApp": {
+                        "id": response_json["id"],
+                        # "installation_id": ,  TODO: include or omit?
+                        # "webhook_url": response_json, TODO: get this from API?
+                        "webhook_secret": response_json["webhook_secret"],
+                        "private_key": response_json["pem"],
+                    }
+                }
+                with open(f"{CACHEDIR}/github_app_config.dev.yaml", "w") as f:
+                    yaml.dump(app_config, f)
+
+            return http.server.SimpleHTTPRequestHandler.do_GET(self)
+
+    with socketserver.TCPServer(("", PORT), Handler) as httpd:
+        print("To authorize a new dev app, navigate to", authorize_url)
+        httpd.serve_forever()
