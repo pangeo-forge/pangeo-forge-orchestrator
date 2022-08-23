42d5fa7d8a477057c4232f4041177a321150d599
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-08-23 13:19:08 -0700

    generalize init database script

--- scripts/initialize_database.py
+++ scripts/initialize_database.py
@@ -8,20 +8,24 @@ from pangeo_forge_orchestrator.config import get_config
 c = get_config()
 
 if __name__ == "__main__":
-    port, spec = sys.argv[1:3]
+    # `app_address` might be http://localhost:8000 for a local instance, or e.g.
+    #   https://pangeo-forge-api-pr-80.herokuapp.com for a review app instance.
+    # `spec` is the full name of a mock feedstock, e.g. `cisaacstern/mock-dataset-feedstock`;
+    #   the repo indicated by this spec must actually exist on GitHub.
+    app_address, spec = sys.argv[1:3]
     headers = {
         "X-API-Key": c.fastapi.PANGEO_FORGE_API_KEY,
         "accept": "application/json",
         "Content-Type": "application/json",
     }
     feedstock_response = requests.post(
-        f"http://localhost:{port}/feedstocks/",
+        f"{app_address}/feedstocks/",
         headers=headers,
         data=json.dumps({"spec": spec}),
     )
     feedstock_response.raise_for_status()
     bakery_response = requests.post(
-        f"http://localhost:{port}/bakeries/",
+        f"{app_address}/bakeries/",
         headers=headers,
         data=json.dumps(
             {
