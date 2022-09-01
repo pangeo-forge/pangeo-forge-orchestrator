5cdd108323486f7b0584a94a91be3ca36f9b74f4
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-09-01 13:10:44 -0700

    update release script for remote gcp state

--- scripts.deploy/release.sh
+++ scripts.deploy/release.sh
@@ -10,7 +10,6 @@ export APP_1="pforge-pr-80"
 export TF_IN_AUTOMATION=true
 export TF_DIR="./dataflow-status-monitoring/terraform"
 export TF_CREDS="./secrets/dataflow-status-monitoring.json"
-export TF_STATE="./secrets/terraform.tfstate"
 export GET_WEBHOOK_SECRET="import sys, yaml; print(yaml.safe_load(sys.stdin)['github_app']['webhook_secret'].strip())"
 export GET_GCP_PROJECT="import sys, json; print(json.load(sys.stdin)['project_id'].strip())"
 
@@ -31,8 +30,7 @@ sops -d -i ${TF_CREDS}
 export GCP_PROJECT=$(cat ./${TF_CREDS} | python3.9 -c "${GET_GCP_PROJECT}")
 
 echo "running terraform..."
-sops -d -i ${TF_STATE}
-terraform -chdir=${TF_DIR} init -backend-config 'path=../.'${TF_STATE}
+terraform -chdir=${TF_DIR} init
 terraform -chdir=${TF_DIR} plan -out tfplan \
 -var 'credentials_file=../.'${TF_CREDS} \
 -var 'project='${GCP_PROJECT} \
@@ -49,7 +47,6 @@ else
 fi
 
 sops -e -i ${TF_CREDS}
-sops -e -i ${TF_STATE}
 # TODO: do this in a loop
 sops -e -i "./secrets/config.${APP_0}.yaml"
 sops -e -i "./secrets/config.${APP_1}.yaml"
