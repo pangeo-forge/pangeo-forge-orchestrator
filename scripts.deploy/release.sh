cff9ef10d94f0da8f7b06fb5b653c1df9c190800
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-09-02 12:45:58 -0700

    decrypt bakery secrets on *run*, not release

--- scripts.deploy/release.sh
+++ scripts.deploy/release.sh
@@ -63,9 +63,4 @@ for app in ${app_array[@]}; do
   sops -e -i "./secrets/config.${app}.yaml"
 done
 
-echo "decrypting bakery secrets..."
-# TODO: make this a loop over files starting with 'secrets/bakery-env',
-# so we don't need to know the names ahead of time
-sops -d -i "./secrets/bakery-args.pangeo-ldeo-nsf-earthcube.yaml"
-
 echo "release complete!"
