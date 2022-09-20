1d4c57344cdd3c820a8c0b4889cf05f76a792600
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-09-20 13:51:46 -0700

    use per-test gh_backend fixture for synchronize

--- tests/github_app/fixtures.py
+++ tests/github_app/fixtures.py
@@ -27,66 +27,81 @@ def add_hash_signature(request: dict, webhook_secret: str):
     return request
 
 
-@pytest.fixture
-def api_url():
-    """In production, this might be configured to point to a different url, for example for
-    testing new features on a review deployment."""
-
-    return "https://api.pangeo-forge.org"
-
-
-@pytest.fixture
-def app_hook_config_url(api_url):
-    return f"{api_url}/github/hooks/"
-
-
-@pytest.fixture
-def accessible_repos():
-    """The repositories in which the app has been installed."""
-
-    return [{"full_name": "pangeo-forge/staged-recipes"}]
-
-
-@pytest.fixture
-def app_installations():
-    """Installations for the mock GitHub App. The real payload contains a lot more information, but
-    just including the subset that we use here.
-    """
-
-    return [{"id": 1234567}]
-
-
-@pytest.fixture
-def mock_github_backend(
-    app_hook_config_url,
-    accessible_repos,
-    app_hook_deliveries,
-    app_installations,
-    staged_recipes_pulls_files,
-):
-    """The backend data which simulates data that is retrievable via the GitHub API. Importantly,
-    this has to be its own fixture, so that if multiple instances of the ``MockGitHubAPI`` are
-    used within a single test invocation, each of these sessions share a single instance of the
-    backend data. Multiple ``MockGitHubAPI`` sessions *are* used in certain test invocations,
-    because sometimes we use one session to populate pre-requisite data, and then the function
-    under test starts a separate session to query that data.
-    """
-
-    backend_kws = {
-        "_app_hook_config_url": app_hook_config_url,
-        "_accessible_repos": accessible_repos,
-        "_app_hook_deliveries": app_hook_deliveries,
-        "_app_installations": app_installations,
-        "_check_runs": list(),
-        "_pulls_files": {
-            "pangeo-forge/staged-recipes": staged_recipes_pulls_files,
-        },
-    }
-    return _MockGitHubBackend(**backend_kws)
-
-
-def get_mock_github_session(mock_github_backend):
+def get_mock_github_session(mock_github_backend: _MockGitHubBackend):
     def _get_mock_github_session(http_session: HttpSession):
         return MockGitHubAPI(http_session, "pangeo-forge", _backend=mock_github_backend)
 
     return _get_mock_github_session
+
+
+@pytest.fixture
+def staged_recipes_pr_1_files():
+    return [
+        {
+            "filename": "recipes/new-dataset/recipe.py",
+            "contents_url": (
+                "https://api.github.com/repos/contributor-username/staged-recipes/"
+                "contents/recipes/new-dataset/recipe.py"
+            ),
+            "sha": "abcdefg",
+        },
+        {
+            "filename": "recipes/new-dataset/meta.yaml",
+            "contents_url": (
+                "https://api.github.com/repos/contributor-username/staged-recipes/"
+                "contents/recipes/new-dataset/meta.yaml"
+            ),
+            "sha": "abcdefg",
+        },
+    ]
+
+
+@pytest.fixture
+def staged_recipes_pr_2_files(staged_recipes_pr_1_files):
+    # This PR is the automated cleanup PR following merge of PR 1. I think that means the
+    # `files` JSON is more-or-less the same? Except that the contents would be different,
+    # of course, but our fixtures don't capture that level of detail yet.
+    return staged_recipes_pr_1_files
+
+
+@pytest.fixture
+def staged_recipes_pr_3_files():
+    return [
+        {
+            "filename": "README.md",
+            "contents_url": (
+                "https://api.github.com/repos/contributor-username/staged-recipes/"
+                "contents/README.md"
+            ),
+            "sha": "abcdefg",
+        },
+    ]
+
+
+@pytest.fixture
+def staged_recipes_pr_4_files():
+    return [
+        {
+            "filename": "README.md",
+            "contents_url": (
+                "https://api.github.com/repos/contributor-username/dataset-feedstock/"
+                "contents/README.md"
+            ),
+            "sha": "abcdefg",
+        },
+    ]
+
+
+@pytest.fixture
+def staged_recipes_pulls_files(
+    staged_recipes_pr_1_files,
+    staged_recipes_pr_2_files,
+    staged_recipes_pr_3_files,
+    staged_recipes_pr_4_files,
+):
+    return {
+        1: staged_recipes_pr_1_files,
+        2: staged_recipes_pr_2_files,
+        3: staged_recipes_pr_3_files,
+        4: staged_recipes_pr_4_files,
+    }
--- tests/github_app/test_POST_synchronize_event.py
+++ tests/github_app/test_POST_synchronize_event.py
@@ -1,14 +1,15 @@
-import os
 import subprocess
 from typing import List
+from urllib.parse import urlparse
 
 import pytest
 import pytest_asyncio
 
 import pangeo_forge_orchestrator
+from pangeo_forge_orchestrator.routers.github_app import DEFAULT_BACKEND_NETLOC
 
 from ..conftest import clear_database
-from .fixtures import add_hash_signature
+from .fixtures import _MockGitHubBackend, add_hash_signature, get_mock_github_session
 
 
 def mock_subprocess_check_output(cmd: List[str]):
@@ -38,12 +39,24 @@ def mock_subprocess_check_output(cmd: List[str]):
         )
 
 
+@pytest.fixture(params=["https://api.pangeo-forge.org", "https://api-staging.pangeo-forge.org"])
+def api_url(request):
+    return request.param
+
+
+@pytest.fixture
+def app_hook_config_url(api_url):
+    return f"{api_url}/github/hooks/"
+
+
 @pytest_asyncio.fixture
-async def synchronize_request(
+async def synchronize_request_fixture(
     webhook_secret,
     async_app_client,
     admin_key,
     request,
+    staged_recipes_pulls_files,
+    app_hook_config_url,
 ):
     headers = {"X-GitHub-Event": "pull_request"}
     payload = {
@@ -89,7 +102,65 @@ async def synchronize_request(
     )
     assert feedstock_create_response.status_code == 200
 
-    yield add_hash_signature(request, webhook_secret)
+    gh_backend_kws = {
+        "_app_installations": [{"id": 1234567}],
+        "_accessible_repos": [
+            {"full_name": "pangeo-forge/staged-recipes"},
+            {"full_name": "pangeo-forge/pangeo-forge.org"},
+        ],
+        "_app_hook_config_url": app_hook_config_url,
+        "_check_runs": list(),
+        "_pulls_files": {
+            "pangeo-forge/staged-recipes": staged_recipes_pulls_files,
+        },
+    }
+    expected_recipe_runs_response = [
+        {
+            "recipe_id": "gpcp",
+            "bakery_id": 1,
+            "feedstock_id": 1,
+            "head_sha": "abc",
+            "version": "",
+            "started_at": "2022-08-11T21:03:56",
+            "completed_at": None,
+            "conclusion": None,
+            "status": "queued",
+            "is_test": True,
+            "dataset_type": "zarr",
+            "dataset_public_url": None,
+            "message": None,
+            "id": 1,
+        }
+    ]
+    frontend_url = "https://pangeo-forge.org/dashboard/recipe-run/1?feedstock_id=1"
+    if DEFAULT_BACKEND_NETLOC not in app_hook_config_url:
+        frontend_url += f"&orchestratorEndpoint={urlparse(app_hook_config_url).netloc}"
+    expected_check_runs_response = {
+        "total_count": 1,
+        "check_runs": [
+            {
+                "name": "synchronize",
+                "head_sha": "abc",
+                "status": "completed",
+                "started_at": "2022-08-11T21:22:51Z",
+                "output": {
+                    "title": "Recipe runs queued for latest commit",
+                    "summary": f"Recipe runs created at commit `abc`:\n- {frontend_url}",
+                },
+                "details_url": "https://pangeo-forge.org/",
+                "id": 0,
+                "conclusion": "success",
+                "completed_at": "2022-08-11T21:22:51Z",
+            }
+        ],
+    }
+
+    yield (
+        add_hash_signature(request, webhook_secret),
+        _MockGitHubBackend(**gh_backend_kws),
+        expected_recipe_runs_response,
+        expected_check_runs_response,
+    )
 
     # database teardown
     clear_database()
@@ -97,25 +168,28 @@ async def synchronize_request(
 
 @pytest.mark.asyncio
 @pytest.mark.parametrize(
-    "synchronize_request",
+    "synchronize_request_fixture",
     ["pangeo-forge/staged-recipes", "pangeo-forge/pangeo-forge.org"],
     indirect=True,
 )
 async def test_receive_synchronize_request(
     mocker,
-    get_mock_github_session,
-    webhook_secret,
     async_app_client,
-    synchronize_request,
-    private_key,
+    synchronize_request_fixture,
 ):
-    os.environ["GITHUB_WEBHOOK_SECRET"] = webhook_secret
+    (
+        synchronize_request,
+        gh_backend,
+        expected_recipe_runs_response,
+        expected_check_runs_response,
+    ) = synchronize_request_fixture
     mocker.patch.object(
-        pangeo_forge_orchestrator.routers.github_app, "get_github_session", get_mock_github_session
+        pangeo_forge_orchestrator.routers.github_app,
+        "get_github_session",
+        get_mock_github_session(gh_backend),
     )
 
     mocker.patch.object(subprocess, "check_output", mock_subprocess_check_output)
-    os.environ["PEM_FILE"] = private_key
     response = await async_app_client.post(
         "/github/hooks/",
         json=synchronize_request["payload"],
@@ -131,26 +205,6 @@ async def test_receive_synchronize_request(
         # first assert that the recipe runs were created as expected
         recipe_runs_response = await async_app_client.get("/recipe_runs/")
         assert recipe_runs_response.status_code == 200
-        # TODO: fixturize expected_recipe_runs_response
-        expected_recipe_runs_response = [
-            {
-                "recipe_id": "gpcp",
-                "bakery_id": 1,
-                "feedstock_id": 1,
-                "head_sha": "abc",
-                "version": "",
-                "started_at": "2022-08-11T21:03:56",
-                "completed_at": None,
-                "conclusion": None,
-                "status": "queued",
-                "is_test": True,
-                "dataset_type": "zarr",
-                "dataset_public_url": None,
-                "message": None,
-                "id": 1,
-            }
-        ]
-
         for k in expected_recipe_runs_response[0]:
             if k not in ["started_at", "completed_at"]:
                 assert expected_recipe_runs_response[0][k] == recipe_runs_response.json()[0][k]
@@ -160,27 +214,6 @@ async def test_receive_synchronize_request(
         check_runs_response = await async_app_client.get(
             f"/feedstocks/1/commits/{commit_sha}/check-runs"
         )
-
-        # TODO: fixturize expected_check_runs_response
-        expected_check_runs_response = {
-            "total_count": 1,
-            "check_runs": [
-                {
-                    "name": "synchronize",
-                    "head_sha": "abc",
-                    "status": "completed",
-                    "started_at": "2022-08-11T21:22:51Z",
-                    "output": {
-                        "title": "Recipe runs queued for latest commit",
-                        "summary": "Recipe runs created at commit `abc`:\n- https://pangeo-forge.org/dashboard/recipe-run/1?feedstock_id=1",
-                    },
-                    "details_url": "https://pangeo-forge.org/",
-                    "id": 0,
-                    "conclusion": "success",
-                    "completed_at": "2022-08-11T21:22:51Z",
-                }
-            ],
-        }
 
         assert expected_check_runs_response["total_count"] == 1
         for k in expected_check_runs_response["check_runs"][0]:
