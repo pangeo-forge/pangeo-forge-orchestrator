2e93fe6d71390f19c38ef3bf0841f05a93a19600
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-09-21 11:29:21 -0700

    test make_dataflow_job_name

--- tests/github_app/test_helpers.py
+++ tests/github_app/test_helpers.py
@@ -1,3 +1,6 @@
+from typing import Tuple
+from urllib.parse import urlparse
+
 import jwt
 import pytest
 from gidgethub.aiohttp import GitHubAPI
@@ -15,6 +18,7 @@ from pangeo_forge_orchestrator.routers.github_app import (
     html_to_api_url,
     html_url_to_repo_full_name,
     list_accessible_repos,
+    make_dataflow_job_name,
 )
 
 from .fixtures import _MockGitHubBackend, get_mock_github_session
@@ -118,6 +122,86 @@ async def test_list_accessible_repos():
     mock_gh = get_mock_github_session(gh_backend)(http_session)
     repos = await list_accessible_repos(mock_gh)
     assert repos == [r["full_name"] for r in accessible_repos]
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("recipe_run_id", [1, 9999, 10_000, 9_999_999, 10_000_000])
+async def test_make_dataflow_job_name(app_hook_config_url, recipe_run_id):
+    recipe_run_kws = {
+        "recipe_id": "liveocean",
+        "bakery_id": 1,
+        "feedstock_id": 1,
+        "head_sha": "35d889f7c89e9f0d72353a0649ed1cd8da04826b",
+        "version": "",
+        "started_at": "2022-09-19T16:31:43",
+        "completed_at": None,
+        "conclusion": None,
+        "status": "in_progress",
+        "is_test": True,
+        "dataset_type": "zarr",
+        "dataset_public_url": None,
+        "message": None,
+        "id": recipe_run_id,
+    }
+    recipe_run = MODELS["recipe_run"].table(**recipe_run_kws)
+    gh_backend_kws = {
+        "_app_hook_config_url": app_hook_config_url,
+    }
+    gh_backend = _MockGitHubBackend(**gh_backend_kws)
+    mock_gh = get_mock_github_session(gh_backend)(http_session)
+
+    def reverse_encoding(job_name: str) -> Tuple[str, int]:
+        # TODO: This code is duplicative of the code used in the submodule
+        # `dataflow-status-monitoring/src/main.py` to decode job names. Ideally, we should be using
+        # the *actual* decoding function to test here, however the submodule code is not currently
+        # factored in such a way that makes that possible. (That is, decoding job names is done as
+        # part of a monolithic function there. If it were it's own function, we could import it
+        # here.) The decode behavior should be definitely be factored into a distinct functions and
+        # probably be situated in the same module as the encoder. I'm not yet clear where these
+        # paired functions should live, however:
+        #   1. Together in `dataflow-status-monitoring`?
+        #   2. Together in `pangeo-forge-orchestrator`?
+        # I favor the latter, but if we go this route, how does `dataflow-status-monitoring` access
+        # the decoder function without a circular dependency? Perhaps this points to just merging
+        # `dataflow-status-monitoring` code into `pangeo-forge-orchestrator`.
+        job_name = job_name[1:]  # drop leading 'a' from job_name
+        as_pairs = [a + b for a, b in zip(job_name[::2], job_name[1::2])]
+        control_character_idx = [
+            i for i, val in enumerate(as_pairs) if chr(int(val, 16)) == "%"
+        ].pop(0)
+        recipe_run_id = int("".join(as_pairs[control_character_idx + 1 :]))
+        # NOTE: in `dataflow-status-monitoring` this variable is named `webhook_url`, but I realized
+        # as a I was writing this test, that it's more accurately named `*_netloc`, rather than `url`
+        decoded_netloc = "".join([chr(int(val, 16)) for val in as_pairs[:control_character_idx]])
+        return decoded_netloc, recipe_run_id
+
+    app_hook_netloc = urlparse(app_hook_config_url).netloc
+    if len(app_hook_netloc) + len(str(recipe_run_id)) > 32:
+        # the current encoding scheme requires 2 characters for every 1 character in the decoded
+        # content. therefore, if the decoded content exceeds 32 chars in length, then we cannot
+        # create a valid dataflow job name (which cannot be > 64 chars) for this content. in
+        # practice, with our current DNS names, this means:
+        #   1. we cannot have > 9999 recipe_runs in the staging database
+        #   2. we cannot have > 9_999_999 recipe_runs in the production database
+        # which seems tenable. the greater number of allowable recipe_runs in the prod database
+        # is due to the fact that the DNS name for prod API is 8 characters shorter than the DNS
+        # name for the staging API, therefore we gain 8 additional characters (i.e. digits) of
+        # encodable content length.
+        assert recipe_run_id in (10_000, 9_999_999, 10_000_000)
+
+        if app_hook_netloc == "api.pangeo-forge.org":
+            assert recipe_run_id > 9_999_999
+        elif app_hook_netloc == "api-staging.pangeo-forge.org":
+            assert any([recipe_run_id == n for n in (10_000, 9_999_999, 10_000_000)])
+
+        with pytest.raises(ValueError, match=r"exceeds max dataflow job name len of 64 chars.$"):
+            _ = await make_dataflow_job_name(recipe_run, mock_gh)
+
+    else:
+        job_name = await make_dataflow_job_name(recipe_run, mock_gh)
+        decoded_netloc, recipe_run_id = reverse_encoding(job_name)
+        assert f"https://{decoded_netloc}/github/hooks/" == app_hook_config_url
+        assert recipe_run_id == recipe_run.id
 
 
 # TODO: the functions tested by the next two tests should be removed.
