4167666cb302fb63e56931f3e9679c7c47464675
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-09-29 16:57:01 -0700

    logs router cont.

--- pangeo_forge_orchestrator/routers/logs.py
+++ pangeo_forge_orchestrator/routers/logs.py
@@ -1,12 +1,38 @@
+import json
 import subprocess
 
-from fastapi import APIRouter, Depends, Query
+from fastapi import APIRouter, Depends, HTTPException, Query
 from fastapi.responses import PlainTextResponse
-from sqlmodel import Session, select
+from sqlmodel import Session, SQLModel, select
+from starlette import status
 
 from ..dependencies import check_authentication_header, get_session as get_database_session
+from ..models import MODELS
 
 logs_router = APIRouter()
+
+DEFAULT_SEVERITY = Query(
+    "ERROR",
+    description=(
+        "A valid gcloud logging severity as defined in "
+        "https://cloud.google.com/logging/docs/reference/v2/rest/v2/LogEntry#LogSeverity"
+    ),
+)
+DEFAULT_LIMIT = Query(1, description="Max number of log entries to return.")
+
+
+def job_id_from_recipe_run(recipe_run: SQLModel) -> str:
+    try:
+        job_id = json.loads(recipe_run.message)["job_id"]
+    except (KeyError, json.JSONDecodeError) as e:
+        detail = (
+            f"Message field of {recipe_run = } missing 'job_id'."
+            if type(e) == KeyError
+            else f"Message field of {recipe_run = } not JSON decodable."
+        )
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=detail)
+
+    return job_id
 
 
 def get_logs(
@@ -14,12 +40,11 @@ def get_logs(
     severity="ERROR",  # set from query
     limit=1,  # set from query
 ):
+    log_name_prefix = "projects/pangeo-forge-4967/logs/dataflow.googleapis.com"
     query = (
         'resource.type="dataflow_step" '
         f'AND resource.labels.job_id="{job_id}" '
-        "AND logName=("
-        '"projects/pangeo-forge-4967/logs/dataflow.googleapis.com%2Fjob-message" '
-        'OR "projects/pangeo-forge-4967/logs/dataflow.googleapis.com%2Flauncher") '
+        f'AND logName=("{log_name_prefix}%2Fjob-message" OR "{log_name_prefix}%2Flauncher") '
         f'AND severity="{severity}"'
     )
     cmd = "gcloud logging read".split() + [query]
@@ -27,9 +52,7 @@ def get_logs(
     if limit:
         cmd += [f"--limit={limit}"]
 
-    print(cmd)
     logs = subprocess.check_output(cmd)
-
     return logs
 
 
@@ -42,25 +65,45 @@ def get_logs(
 )
 async def logs_from_recipe_run_id(
     id: int,
+    *,
     db_session: Session = Depends(get_database_session),
+    severity: str = DEFAULT_SEVERITY,
+    limit: int = DEFAULT_LIMIT,
 ):
-    logs = get_logs()
+    recipe_run = db_session.exec(
+        select(MODELS["recipe_run"].table).where(MODELS["recipe_run"].table.id == id)
+    ).one()
+    job_id = job_id_from_recipe_run(recipe_run)
+    logs = get_logs(job_id, severity, limit)
     return logs
 
 
 @logs_router.get(
-    "/feedstocks/{org}/{repo}/{commit}/{recipe_id}/logs",
+    "/feedstocks/{feedstock_spec:path}/{commit}/{recipe_id}/logs",
     summary="Get job logs for a recipe run, specified by feedstock_spec, commit, and recipe_id.",
     tags=["feedstock", "logs", "admin"],
     response_class=PlainTextResponse,
     dependencies=[Depends(check_authentication_header)],
 )
 async def logs_from_feedstock_spec_commit_and_recipe_id(
-    org: str,
-    repo: str,
+    feedstock_spec: str,
     commit: str,
     recipe_id: str,
+    *,
     db_session: Session = Depends(get_database_session),
+    severity: str = DEFAULT_SEVERITY,
+    limit: int = DEFAULT_LIMIT,
 ):
-    logs = get_logs()
+    feedstock = db_session.exec(
+        select(MODELS["feedstock"].table).where(MODELS["feedstock"].table.spec == feedstock_spec)
+    ).one()
+    statement = (
+        select(MODELS["recipe_run"].table)
+        .where(MODELS["recipe_run"].table.recipe_id == recipe_id)
+        .where(MODELS["recipe_run"].table.head_sha == commit)
+        .where(MODELS["recipe_run"].table.feedstock_id == feedstock.id)
+    )
+    recipe_run = db_session.exec(statement).one()
+    job_id = job_id_from_recipe_run(recipe_run)
+    logs = get_logs(job_id, severity, limit)
     return logs
