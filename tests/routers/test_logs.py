79204aee4474d551c23c27fd28d86f985eafe494
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-09-29 18:23:05 -0700

    test get logs route

--- tests/routers/test_logs.py
+++ tests/routers/test_logs.py
@@ -3,10 +3,13 @@ import subprocess
 from typing import List
 
 import pytest
+import pytest_asyncio
 from fastapi import HTTPException
 
 from pangeo_forge_orchestrator.models import MODELS
 from pangeo_forge_orchestrator.routers.logs import get_logs, job_id_from_recipe_run
+
+from ..conftest import clear_database
 
 
 @pytest.mark.parametrize(
@@ -59,3 +62,133 @@ def test_get_logs(mocker, gcloud_logging_response):
         limit=1,
     )
     assert logs == gcloud_logging_response
+
+
+@pytest_asyncio.fixture
+async def get_logs_fixture(
+    api_key,
+    async_app_client,
+    request,
+):
+    admin_headers = {"X-API-Key": api_key}
+    bakery_create_response = await async_app_client.post(
+        "/bakeries/",
+        json={
+            "region": "us-central1",
+            "name": "pangeo-ldeo-nsf-earthcube",
+            "description": "A great bakery to test with!",
+        },
+        headers=admin_headers,
+    )
+    assert bakery_create_response.status_code == 200
+    feedstock_create_response = await async_app_client.post(
+        "/feedstocks/",
+        json={"spec": request.param["feedstock_spec"]},
+        headers=admin_headers,
+    )
+    assert feedstock_create_response.status_code == 200
+    recipe_run_kws = {
+        "recipe_id": request.param["recipe_id"],
+        "bakery_id": 1,
+        "feedstock_id": 1,
+        "head_sha": request.param["commit"],
+        "version": "",
+        "started_at": "2022-09-19T16:31:43",
+        "completed_at": None,
+        "conclusion": None,
+        "status": "in_progress",
+        "is_test": True,
+        "dataset_type": "zarr",
+        "dataset_public_url": None,
+        "message": request.param["message"],
+        "id": 1,
+    }
+    recipe_run_response = await async_app_client.post(
+        "/recipe_runs/",
+        json=recipe_run_kws,
+        headers=admin_headers,
+    )
+    assert recipe_run_response.status_code == 200
+    yield (
+        admin_headers,
+        request.param["gcloud_logging_response"],
+        request.param["feedstock_spec"],
+        request.param["commit"],
+        request.param["recipe_id"],
+    )
+    # database teardown
+    clear_database()
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "get_logs_fixture",
+    [
+        dict(
+            message='{"job_id": "abc"}',
+            feedstock_spec="pangeo-forge/staged-recipes",
+            commit="35d889f7c89e9f0d72353a0649ed1cd8da04826b",
+            recipe_id="liveocean",
+            gcloud_logging_response="Some logging message from gcloud API.",
+        ),
+    ],
+    indirect=True,
+)
+async def test_get_logs_via_recipe_run_id(
+    mocker,
+    get_logs_fixture,
+    async_app_client,
+):
+    admin_headers, gcloud_logging_response, *_ = get_logs_fixture
+
+    def mock_gcloud_logging_call(cmd: List[str]):
+        return gcloud_logging_response
+
+    mocker.patch.object(subprocess, "check_output", mock_gcloud_logging_call)
+
+    response = await async_app_client.get(
+        "/recipe_runs/1/logs",
+        headers=admin_headers,
+    )
+    assert response.status_code == 200
+    assert response.text == gcloud_logging_response
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "get_logs_fixture",
+    [
+        dict(
+            message='{"job_id": "abc"}',
+            feedstock_spec="pangeo-forge/staged-recipes",
+            commit="35d889f7c89e9f0d72353a0649ed1cd8da04826b",
+            recipe_id="liveocean",
+            gcloud_logging_response="Some logging message from gcloud API.",
+        ),
+    ],
+    indirect=True,
+)
+async def test_get_logs_human_readable_method(
+    mocker,
+    get_logs_fixture,
+    async_app_client,
+):
+    (
+        admin_headers,
+        gcloud_logging_response,
+        feedstock_spec,
+        commit,
+        recipe_id,
+    ) = get_logs_fixture
+
+    def mock_gcloud_logging_call(cmd: List[str]):
+        return gcloud_logging_response
+
+    mocker.patch.object(subprocess, "check_output", mock_gcloud_logging_call)
+
+    response = await async_app_client.get(
+        f"/feedstocks/{feedstock_spec}/{commit}/{recipe_id}/logs",
+        headers=admin_headers,
+    )
+    assert response.status_code == 200
+    assert response.text == gcloud_logging_response
