5dffb3808922add4e39c4def2facf5bfc9d8ee2f
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-12-07 12:10:39 -0800

    add server trait to fastapi configurable

--- pangeo_forge_orchestrator/commands/up.py
+++ pangeo_forge_orchestrator/commands/up.py
@@ -1,4 +1,4 @@
-from ..configurables.github_app import GitHubApp
+from ..configurables.fastapi import FastAPI
 from .base import BaseCommand, common_aliases
 
 
@@ -11,9 +11,5 @@ class Up(BaseCommand):
 
         self.log.info("Going up!")
 
-        github_app = GitHubApp(parent=self)
-        self.log.info(github_app)
-        self.log.info(github_app.app_name)
-        self.log.info(github_app.id)
-        self.log.info(github_app.private_key)
-        self.log.info(github_app.webhook_secret)
+        fastapi = FastAPI(parent=self)
+        fastapi.server.start()
