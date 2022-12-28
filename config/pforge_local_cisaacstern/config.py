711ddb31b4c01ec468d8f5ee1fd7749607f30724
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-12-28 12:36:46 -0800

    mv cisaacstern local bakery config

--- config/pforge_local_cisaacstern/config.py
+++ config/pforge_local_cisaacstern/config.py
@@ -1,5 +1,36 @@
 from traitlets.config import get_config
 
+from .. import get_default_container_image
+
 c = get_config()
 
 c.Deployment.name = "pforge-local-cisaacstern"
+
+
+# TODO: complete this incomplete config
+
+c.Deployment.registered_runner_configs = {
+    "pangeo-ldeo-nsf-earthcube": {
+        "Bake": {
+            "bakery_class": "pangeo_forge_runner.bakery.dataflow.DataflowBakery",
+            "container_image": get_default_container_image(),
+        },
+        "DataflowBakery": {"temp_gcs_location": "gs://pangeo-forge-dev-dataflow/temp"},
+        "TargetStorage": {
+            "fsspec_class": "gcsfs.GCSFileSystem",
+            "fsspec_args": {"bucket": "pangeo-forge-dev-target"},
+            "root_path": "pangeo-forge-dev-target/{subpath}",
+            "public_url": "https://storage.googleapis.com/{root_path}",
+        },
+        "InputCacheStorage": {
+            "fsspec_class": "gcsfs.GCSFileSystem",
+            "fsspec_args": {"bucket": "pangeo-forge-dev-cache"},
+            "root_path": "pangeo-forge-dev-cache",
+        },
+        "MetadataCacheStorage": {
+            "fsspec_class": "gcsfs.GCSFileSystem",
+            "fsspec_args": {},
+            "root_path": "pangeo-forge-dev-cache/metadata/{subpath}",
+        },
+    }
+}
