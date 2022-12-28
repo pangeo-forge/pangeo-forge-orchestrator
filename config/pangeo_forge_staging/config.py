ac884e271ecec025a8b6e81c959c435cd853bcf7
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-12-28 12:41:02 -0800

    mv staging bakery config

--- config/pangeo_forge_staging/config.py
+++ config/pangeo_forge_staging/config.py
@@ -1,1 +1,40 @@
-c.Deployment.name = "pangeo-forge-staging"  # type: ignore # noqa: F821
+from traitlets.config import get_config
+
+from .. import get_default_container_image
+
+c = get_config()
+
+c.Deployment.name = "pangeo-forge-staging"
+
+# TODO: complete this incomplete config (don't forget osn creds for runner config below)
+
+c.Deployment.registered_runner_configs = {
+    "pangeo-ldeo-nsf-earthcube": {
+        "Bake": {"bakery_class": "pangeo_forge_runner.bakery.dataflow.DataflowBakery"},
+        "DataflowBakery": {
+            "temp_gcs_location": "gs://pangeo-forge-staging-dataflow/temp",
+            "container_image": get_default_container_image(),
+        },
+        "TargetStorage": {
+            "fsspec_class": "s3fs.S3FileSystem",
+            "fsspec_args": {
+                "client_kwargs": {"endpoint_url": "https://ncsa.osn.xsede.org"},
+                "default_cache_type": "none",
+                "default_fill_cache": False,
+                "use_listings_cache": False,
+            },
+            "root_path": "Pangeo/{subpath}",
+            "public_url": "https://ncsa.osn.xsede.org/{root_path}",
+        },
+        "InputCacheStorage": {
+            "fsspec_class": "gcsfs.GCSFileSystem",
+            "fsspec_args": {"bucket": "pangeo-forge-staging-cache"},
+            "root_path": "pangeo-forge-staging-cache",
+        },
+        "MetadataCacheStorage": {
+            "fsspec_class": "gcsfs.GCSFileSystem",
+            "fsspec_args": {},
+            "root_path": "pangeo-forge-staging-cache/metadata/{subpath}",
+        },
+    }
+}
