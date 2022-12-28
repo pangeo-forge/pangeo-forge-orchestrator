7c57a6a235ca020d1422e53f8f6df6b35970d965
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-12-27 19:35:32 -0800

    drop server stuff from fastapi config

--- pangeo_forge_orchestrator/configurables/fastapi.py
+++ pangeo_forge_orchestrator/configurables/fastapi.py
@@ -1,63 +1,13 @@
-import subprocess
-from pathlib import Path
-
-from traitlets import Int, TraitType, Unicode, validate
+from traitlets import Unicode, validate
 from traitlets.config import LoggingConfigurable
 
 from ._types import SecretStr
-
-root_dir = Path(__file__).parent.parent.parent.resolve()
-
-
-class Server:
-    def start(self):
-        pass
-
-
-class UvicornServer(Server):
-    def start(self):
-        cmd = (
-            "uvicorn pangeo_forge_orchestrator.api:app "
-            f"--reload --reload-dir={root_dir}/pangeo_forge_orchestrator"
-        ).split()
-        subprocess.run(cmd)
-
-
-class GunicornServer(LoggingConfigurable):
-
-    nworkers = Int(
-        2,
-    )
-
-    timeout = Int(
-        300,
-    )
-
-    def start(self):
-        cmd = (
-            "gunicorn "
-            f"-w {self.nworkers} "
-            f"-t {self.timeout} "
-            "-k uvicorn.workers.UvicornWorker "
-            "pangeo_forge_orchestrator.api:app"
-        ).split()
-        subprocess.run(cmd)
 
 
 class FastAPI(LoggingConfigurable):
     """Config for the FastAPI instance."""
 
     key = Unicode(
-        allow_none=False,
-        config=True,
-        help="""
-
-        """,
-    )
-
-    server: Server = TraitType(
-        UvicornServer(),
-        klass=Server,
         allow_none=False,
         config=True,
         help="""
