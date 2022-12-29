58f6fb4f98af33e25cfe21d82b6cba896a229d48
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-12-28 18:53:34 -0800

    add FIXME funcs to review app config

--- config/pforge_pr_204/config.py
+++ config/pforge_pr_204/config.py
@@ -1,6 +1,39 @@
-from traitlets.config import get_config
+from traitlets.config import get_config  # fmt:skip isort:skip
 
-from .. import get_default_container_image, open_secret
+# FIXME #####################################################################
+# from .. import get_default_container_image, open_secret
+#
+# relative imports don't work, so copying helpers from ``..__init__.py`` here
+import os
+from pathlib import Path
+
+import yaml  # type: ignore
+
+root_dir = Path(__file__).parent.parent.resolve()
+
+DEFAULT_GCP_PROJECT = "pangeo-forge-4967"
+
+
+def open_secret(fname: str) -> dict:
+    with open(
+        root_dir
+        / "config"
+        / os.environ["PANGEO_FORGE_DEPLOYMENT"].replace("-", "_")
+        / "secrets"
+        / fname
+    ) as f:
+        s = yaml.safe_load(f)
+        if "sops" in s:
+            raise ValueError(f"File {s} is encrypted. Decrypt then retry.")
+        return s
+
+
+def get_default_container_image():
+    with open(root_dir / "dataflow-container-image.txt") as img_tag:
+        return f"gcr.io/{DEFAULT_GCP_PROJECT}/{img_tag.read().strip()}"
+
+
+# end FIXME #################################################################
 
 c = get_config()
 
