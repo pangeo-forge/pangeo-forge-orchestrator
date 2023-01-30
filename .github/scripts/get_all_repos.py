1abcf30776ee3b7e9f0ce1084f6c18f813ceb890
Anderson Banihirwe <axbanihirwe@ualr.edu>
2023-01-30 11:42:06 -0700

    Add GH workflow for syncing labels across pangeo-forge organisation

---
+++ .github/scripts/get_all_repos.py
@@ -0,0 +1,56 @@
+import json
+import os
+
+import requests
+
+if __name__ == "__main__":
+
+    ORG_NAME = os.environ["ORG_NAME"]
+    GH_API_TOKEN = os.environ["GH_API_TOKEN"]
+
+    query = f"""
+    query ($cursor: String) {{
+    organization(login: "{ORG_NAME}") {{
+        repositories(first: 100, after: $cursor) {{
+        pageInfo {{
+            endCursor
+            hasNextPage
+        }}
+        edges {{
+            node {{
+            name
+            description
+            url
+            isPrivate
+            }}
+        }}
+        }}
+    }}
+    }}
+    """
+
+    headers = {
+        "Authorization": f"Bearer {GH_API_TOKEN}",
+        "Content-Type": "application/json"
+    }
+
+    all_repos = []
+
+    has_next_page = True
+    cursor = None
+
+    while has_next_page:
+        variables = { "cursor": cursor } if cursor else {}
+        response = requests.post("https://api.github.com/graphql", json={ "query": query, "variables": variables }, headers=headers)
+        data = response.json()
+        repos = data["data"]["organization"]["repositories"]["edges"]
+        all_repos.extend(repos)
+        page_info = data["data"]["organization"]["repositories"]["pageInfo"]
+        has_next_page = page_info["hasNextPage"]
+        cursor = page_info["endCursor"]
+
+    # write all_repos to a JSON string
+    repos_json = json.dumps(all_repos)
+    # set the output for GitHub Actions: https://github.com/orgs/community/discussions/28146
+    print(f'::set-output name=repos::{repos_json}')
+
